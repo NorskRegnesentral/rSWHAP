47865c41469bbc07fd8a8e008c779e729862d948
oigard <tor.arne.oigard@nr.no>
2020-01-20 13:57:48 +0100

    Documentation for data

---
+++ R/ERAInterim_data.R
@@ -0,0 +1,22 @@
+
+#' ERA-Interim data
+#'
+#' ERA-Interim data of Significant Wave Height and Sea Level Pressure.
+#'
+#' @docType data
+#'
+#' @usage data(ERAInterim)
+#'
+#' @format An object of class `"cross"`; see [qtl::read.cross()].
+#'
+#' @keywords datasets
+#'
+#' @references
+#' ([PubMed](https://www.ncbi.nlm.nih.gov/pubmed/23979570))
+#'
+#' @source ERA-INTERIM, <https://phenome.jax.org/projects/Moore1b>
+#'
+#' @examples
+#' data(ERAInterim)
+#' summary(SWH)
+#'
